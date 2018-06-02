b7406add097651f046598064f8ce9a75633b1d3c
win32ramnit <manish.cs.du.2013@gmail.com>
2018-06-02 18:07:05 +0530

    second commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,27 @@
+import { DataService } from './../data.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  goals: any;
+
+  constructor(private route: ActivatedRoute, private router: Router, private _data: DataService) { 
+    this.route.params.subscribe(res => console.log(res.id));
+  }
+
+  ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
+  }
+
+  sendMeHome() {
+    this.router.navigate(['']);
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DataService } from './data.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
+  ],
+  providers: [DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
